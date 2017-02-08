56b0522eddb481a140e55547cbd2221e7f664ede
yinlei <yinlei.zhi@gmail.com>
2017-02-08 22:51:18 +0800

    update

---
+++ game/table/timer.lua
@@ -0,0 +1,13 @@
+-------------------------------------------------------------------------------
+-- timer.lua
+-------------------------------------------------------------------------------
+local _PACKAGE = (...):match("^(.+)[%./][^%./]+") or ""
+
+local table_insert, string_len = table.insert, string.len
+
+local INFO_MSG = tengine.INFO_MSG
+local DEBUG_MSG = tengine.DEBUG_MSG
+local ERROR_MSG = tengine.ERROR_MSG
+local p = tengine.p
+
+local _M = require (_PACKAGE.."/table")
